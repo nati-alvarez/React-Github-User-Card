4fa64517443c74dd7c09cff2906677e98126c71b
Natividad Alvarez <tivialvarez@gmail.com>
2020-07-21 12:08:42 -0400

    Completed step 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const API_URL = "https://api.github.com/users/nati-alvarez";
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount(){
+    axios.get(API_URL).then(({user})=>{
+      this.setState({
+        ...this.state, 
+        user: user
+      })
+    }).catch(err=>{
+      console.log(err);
+    })
+  }
+
+  render(){
+    return (
+      <>
+        <h1>Github User Card</h1>
+      </>
+    );
+  }
 }
 
 export default App;
