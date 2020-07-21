a4a300fb4cbb14fbbd5cecb1283fe13e6a39aa29
Natividad Alvarez <tivialvarez@gmail.com>
2020-07-21 12:45:07 -0400

    Completed step 3

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      user: null
+      user: null,
+      followers: null
     }
   }
 
@@ -22,15 +23,29 @@ class App extends React.Component {
     }).catch(err=>{
       console.log(err);
     })
+
+    axios.get(API_URL + "/followers").then(({data})=>{
+      this.setState({
+        ...this.state,
+        followers: data
+      })
+    })
   }
 
   render(){
+    console.log(this.state.followers)
     return (
       <div className="App">
         <h1>Github User Card</h1>
         {this.state.user &&
           <Usercard user={this.state.user}/>
         }
+        <h2>Followers</h2>
+        <div className="followers">
+          {this.state.followers && this.state.followers.map(follower=>{
+            return <Usercard user={follower}/>;
+          }) }
+        </div>
       </div>
     );
   }
